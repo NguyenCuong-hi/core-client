1197eb4fe0e859ffc8027ab2927677e2effd14d5
CuongNv <nvc.nvcuong@gmail.com>
2025-04-16 11:15:54 +0700

    fix unmount page

--- src/layout/MainLayout/DynamicTabs/index.jsx
+++ src/layout/MainLayout/DynamicTabs/index.jsx
@@ -5,27 +5,39 @@ import Default from "views/default";
 
 const DynamicTabContent = () => {
   const { activeTabKey, tabList } = useSelector((state) => state.tab);
-//   const { permissions: userPermissions } = useSelector((state) => state.auth);
-
-  const currentTab = tabList.find((tab) => tab.key === activeTabKey);
-    console.log('tabList', tabList)
-  if (!currentTab) return <div>Tab không tồn tại.</div>;
-
-  // Check quyền
-  const requiredPermission = currentTab.permission;
-  const hasPermission = !requiredPermission || userPermissions.includes(requiredPermission);
-
-  const Component = TabComponents[currentTab.component];
+  // const { permissions: userPermissions } = useSelector((state) => state.auth);
 
   return (
-    <div style={{ height: 'calc(100vh - 95px)' }}> 
-      <Suspense fallback={<div>Loading...</div>}>
-        {hasPermission ? (
-          Component ? <Component /> : <div>Không tìm thấy component.</div>
-        ) : (
-          <Default />
-        )}
-      </Suspense>
+    <div style={{ height: 'calc(100vh - 95px)', position: 'relative' }}>
+      {tabList.map((tab) => {
+        const Component = TabComponents[tab.component];
+        const isActive = tab.key === activeTabKey;
+        // const requiredPermission = tab.permission;
+        // const hasPermission = !requiredPermission || userPermissions.includes(requiredPermission);
+        const hasPermission = true; // Bỏ tạm nếu không dùng auth
+
+        return (
+          <div
+            key={tab.key}
+            style={{
+              display: isActive ? 'block' : 'none',
+              height: '100%',
+              width: '100%',
+              position: 'absolute',
+              top: 0,
+              left: 0
+            }}
+          >
+            <Suspense fallback={<div>Loading...</div>}>
+              {hasPermission ? (
+                Component ? <Component /> : <div>Không tìm thấy component.</div>
+              ) : (
+                <Default />
+              )}
+            </Suspense>
+          </div>
+        );
+      })}
     </div>
   );
 };
