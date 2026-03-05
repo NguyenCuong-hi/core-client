ab2657356d2f340d42d6965973c1657d86ac17a9
CuongNv <nvc.nvcuong@gmail.com>
2026-03-05 16:54:21 +0700

    fix: resolve socket

--- src/services/config.js
+++ src/services/config.js
@@ -3,4 +3,4 @@ const currentPort = window.location.port
 const baseUrl = `${currentHost}${currentPort ? ':' + currentPort : ''}`;
 export let HOST_API_SERVER = 'http://localhost:8080'
 export let HOST_API_SERVER_AUTH = 'http://localhost:8080'
-export let HOST_SOCKET = 'http://localhost:8080'
+export let HOST_SOCKET = 'https://localhost:8080'
