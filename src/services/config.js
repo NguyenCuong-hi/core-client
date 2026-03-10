1d0b41a973052a5a958b89f2d04904ee16109bb1
CuongNv <nvc.nvcuong@gmail.com>
2026-03-10 14:06:01 +0700

    config: HOST_API

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const currentHost = window.location.hostname
 const currentPort = window.location.port
 const baseUrl = `${currentHost}${currentPort ? ':' + currentPort : ''}`;
-export let HOST_API_SERVER = 'http://localhost:8080'
-export let HOST_API_SERVER_AUTH = 'http://localhost:8080'
-export let HOST_SOCKET = 'https://localhost:8080'
+export let HOST_API_SERVER = 'http://8925a605eafbc8df-171-238-182-174.serveousercontent.com'
+export let HOST_API_SERVER_AUTH = 'http://8925a605eafbc8df-171-238-182-174.serveousercontent.com'
+export let HOST_SOCKET = 'http://8925a605eafbc8df-171-238-182-174.serveousercontent.com'
