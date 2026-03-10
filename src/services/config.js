369fc76b8d55f28e13e745e92499ae78817a1359
CuongNv <nvc.nvcuong@gmail.com>
2026-03-10 14:13:55 +0700

    config: HOST_API

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const currentHost = window.location.hostname
 const currentPort = window.location.port
 const baseUrl = `${currentHost}${currentPort ? ':' + currentPort : ''}`;
-export let HOST_API_SERVER = 'http://8925a605eafbc8df-171-238-182-174.serveousercontent.com'
-export let HOST_API_SERVER_AUTH = 'http://8925a605eafbc8df-171-238-182-174.serveousercontent.com'
-export let HOST_SOCKET = 'http://8925a605eafbc8df-171-238-182-174.serveousercontent.com'
+export let HOST_API_SERVER = 'https://mes-admin.serveousercontent.com'
+export let HOST_API_SERVER_AUTH = 'https://mes-admin.serveousercontent.com'
+export let HOST_SOCKET = 'https://mes-admin.serveousercontent.com'
