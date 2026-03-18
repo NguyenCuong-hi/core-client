49d988be18e126cab3a31616cb476748d021cb83
CuongNv <nvc.nvcuong@gmail.com>
2026-03-18 15:42:25 +0700

    fix: cors

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const currentHost = window.location.hostname
 const currentPort = window.location.port
 const baseUrl = `${currentHost}${currentPort ? ':' + currentPort : ''}`;
-export let HOST_API_SERVER = 'http://localhost:8080'
-export let HOST_API_SERVER_AUTH = 'http://localhost:8080'
-export let HOST_SOCKET = 'http://localhost:8080'
+export let HOST_API_SERVER = 'https://localhost:8080'
+export let HOST_API_SERVER_AUTH = 'https://localhost:8080'
+export let HOST_SOCKET = 'https://localhost:8080'
